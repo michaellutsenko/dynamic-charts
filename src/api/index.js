e99b0ac4a28f01c8fd4a047d89e4962ee2252949
Michael Lutsenko <Itheadon@users.noreply.github.com>
2020-10-05 18:00:59 +0300

    Rewrite API and action creators for the new structure

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,27 @@
 const uri = `https://qrng.anu.edu.au/API/jsonI.php?type=uint8`;
 
+/**
+ * Request random numbers from the API.
+ * @param {Number} length The number of values to request from the API.
+ */
 export const fetchPoints = async (length) => {
-  const response = await fetch({
-    uri: uri + `&length=${length}`,
-    options: { method: 'get' },
-  });
+  try {
+    // Wait for the response
+    const response = await fetch(uri + `&length=${length}`, { method: 'get' });
 
-  if (response.ok) {
-    return await response.json();
+    // If there are no error networks
+    if (response.ok) {
+      // Parse the response's body
+      const { data } = await response.json();
+      // response.data is the collection of values we need
+      return data;
+    }
+
+    // If there's a network error, throw the response body as an error object
+    throw await response.json();
+  } catch (err) {
+    console.error(err);
+    // Action creators always expect a result, so we at least return an empty array
+    return [];
   }
-
-  throw await response.json();
 };
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,36 +1,40 @@
 import { fetchPoints } from '../api';
 
 export const initialiseChart = (name, colour) => async (dispatch) => {
-  const response = await fetchPoints(10);
+  const points = await fetchPoints(10);
   dispatch({
     type: 'initialiseChart',
     name,
     colour,
-    points: response.data,
+    points,
   });
 };
 
-export const getPoint = (name) => async (dispatch) => {
+export const getPoint = (name) => async (dispatch, getState) => {
   const response = await fetchPoints(1);
+  const idx = getState().names.findIndex((x) => x === name);
+
   dispatch({
     type: 'addChartPoint',
-    name,
+    idx,
     point: response.data[0],
   });
 };
 
-export const addEmptyPoint = (name) => {
-  return {
+export const addEmptyPoint = (name) => (dispatch, getState) => {
+  const idx = getState().names.findIndex((x) => x === name);
+  dispatch({
     type: 'addChartPoint',
-    name,
+    idx,
     point: null,
-  };
+  });
 };
 
-export const toggleChart = (name, enabled) => {
+export const toggleChart = (name, enabled) => (dispatch, getState) => {
+  const idx = getState().names.findIndex((x) => x === name);
   return {
     type: 'toggleChart',
-    name,
+    idx,
     enabled,
   };
 };
